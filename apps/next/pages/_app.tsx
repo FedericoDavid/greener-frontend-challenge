67458d980e2324a1db7ff518b407a0a159023dbc
federico_enriquez <federico.enriquez@mobbex.com>
2022-07-14 23:38:17 -0300

    starting stup project w/ solito monorepo & native-base

--- apps/next/pages/_app.tsx
+++ apps/next/pages/_app.tsx
@@ -1,25 +1,22 @@
-import { Provider } from 'app/provider'
-import Head from 'next/head'
-import React from 'react'
-import type { SolitoAppProps } from 'solito'
-import 'raf/polyfill'
+import { Provider } from "app/provider";
+import Head from "next/head";
+import React from "react";
+import type { SolitoAppProps } from "solito";
+import "raf/polyfill";
 
 function MyApp({ Component, pageProps }: SolitoAppProps) {
-  return (
-    <>
-      <Head>
-        <title>Cryptocurrencies - Greener</title>
-        <meta
-          name="description"
-          content="Expo + Next.js with Solito"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Provider>
-        <Component {...pageProps} />
-      </Provider>
-    </>
-  )
+   return (
+      <>
+         <Head>
+            <title>Cryptocurrencies - Greener</title>
+            <meta name="description" content="Expo + Next.js with Solito" />
+            <link rel="icon" href="/favicon.ico" />
+         </Head>
+         <Provider>
+            <Component {...pageProps} />
+         </Provider>
+      </>
+   );
 }
 
-export default MyApp
+export default MyApp;
