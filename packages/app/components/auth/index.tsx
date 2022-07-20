f50d1fc5d90c82e8c0a57d078ec120ef16e49a01
Federico Enriquez <fedenri98@gmail.com>
2022-07-20 01:50:29 -0300

    Working on authentication user flow (#1)
    Co-authored-by: federico_enriquez <federico.enriquez@mobbex.com>

--- packages/app/components/auth/index.tsx
+++ packages/app/components/auth/index.tsx
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
-import Signin from "./Signin";
-import Signup from "./Signup";
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
 
 interface AuthProps {}
 
 export const Auth: React.FC<AuthProps> = () => {
-   const [index, setIndex] = useState(false);
+   const [index, setIndex] = useState(true);
 
-   return <div>{!index ? <Signin /> : <Signup />}</div>;
+   return <div>{!index ? <SignIn /> : <SignUp />}</div>;
 };
